70ef874ef9bdc7bf011bd465d4d4d86f77d39d8c
Tisdale511 <tisdalefry@gmail.com>
2020-05-02 18:07:12 -0700

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,9 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+    def change
+        create_table :students do |student|
+            student.string :name
+            # student.integer :grade
+            # student.string :birthdate
+        end
+    end
 end
